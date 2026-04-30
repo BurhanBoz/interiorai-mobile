2b52860d8fb9f97ff8f930173c3ccae0c485cc8e
burhanboz <boz.burhan@gmail.com>
2026-04-30 16:24:15 +0300

    improvements

--- hooks/useImageActions.ts
+++ hooks/useImageActions.ts
@@ -1,6 +1,6 @@
 import { useState, useCallback } from "react";
 import { Alert, Platform } from "react-native";
-import * as FileSystem from "expo-file-system";
+import * as FileSystem from "expo-file-system/legacy";
 import * as MediaLibrary from "expo-media-library";
 import * as Sharing from "expo-sharing";
 import * as Haptics from "expo-haptics";
